20a9aedf83641eb7e6c0313f59cf860d4eb4e88f
kev-de-dev <136710491+kev-de-dev@users.noreply.github.com>
2024-02-13 15:28:59 +0100

    parametrage environement

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // Assurez-vous d'importer également le fichier CSS pour les animations
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('section');
+
+    sections.forEach(section => {
+        const id = section.getAttribute('id');
+        const title = section.querySelector('h1').textContent;
+        const description = section.querySelector('p').textContent;
+        const links = [];
+
+        section.querySelectorAll('ul li a').forEach(link => {
+            const href = link.getAttribute('href');
+            const text = link.textContent;
+            links.push({ href, text });
+        });
+
+        console.log(`Section ID: ${id}`);
+        console.log(`Title: ${title}`);
+        console.log(`Description: ${description}`);
+        console.log(`Links:`, links);
+        console.log('--------------------');
+    });
+});
